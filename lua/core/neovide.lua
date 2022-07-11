56f8b6ecf65cf85dd6af1b338c8131e2b2bb0bd8
nullishamy <amy.codes@null.net>
2022-07-11 22:42:12 +0100

    fix: disable neovide vfx

--- lua/core/neovide.lua
+++ lua/core/neovide.lua
@@ -2,11 +2,12 @@ local g = vim.g
 local o = vim.o
 
 g.neovide_refresh_rate = 144
-g.neovide_cursor_vfx_particle_lifetime = 1.2
-g.neovide_cursor_vfx_particle_density = 7.0
-g.neovide_cursor_vfx_particle_speed = 10.0
-g.neovide_cursor_vfx_particle_phase = 1.5
-g.neovide_cursor_vfx_particle_curl = 1.0
-g.neovide_cursor_trail_length = 0.1
-g.neovide_cursor_animation_length = 0.1
+
+-- Disable the cursor, it's annoying and has weird visual glitches with Trouble
+g.neovide_cursor_animation_length = 0
 o.guifont = 'JetBrains Mono, monospace:h12'
+
+
+
+
+
