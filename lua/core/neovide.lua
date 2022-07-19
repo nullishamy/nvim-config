d8b9874ca15d24c9eb4ec6ce4d623f1ab504acd2
nullishamy <amy.codes@null.net>
2022-07-19 23:06:57 +0100

    fix: neovide

--- lua/core/neovide.lua
+++ lua/core/neovide.lua
@@ -1,13 +1,10 @@
 local g = vim.g
 local o = vim.o
 
-g.neovide_refresh_rate = 144
+if (g.neovide) then
+  g.neovide_refresh_rate = 144
 
--- Disable the cursor, it's annoying and has weird visual glitches with Trouble
-g.neovide_cursor_animation_length = 0
-o.guifont = 'Comic Code Ligatures, monospace:h13'
-
-
-
-
-
+  -- Disable the cursor, it's annoying and has weird visual glitches with Trouble
+  g.neovide_cursor_animation_length = 0
+  o.guifont = 'Comic Code Ligatures, monospace:h13'
+end
